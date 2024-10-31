03c35c8bcff8915a29da5ddedc7f0c63f9ed4cbc
Yihen <liuyihen@gmail.com>
2024-10-31 11:33:21 +0800

    support getAddressWithoutAmount and getAddressWithAmount

--- router/jsonrpc.go
+++ router/jsonrpc.go
@@ -30,25 +30,85 @@ type addressUtxoResponse struct {
 	Status UtxoStatus `json:"status"`
 }
 
-func GetAddressUtxo(c *gin.Context) {
-	address := c.Param("address")
-	/*
-		var request addressUtxoRequest
-		if err := c.ShouldBindJSON(&request); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-	*/
-	client, err := electrum.NewClientTCP(context.Background(), "node.sathub.io:60601")
+type mempoolAddressUtxoResponse struct {
+	//Height uint32     `json:"height"`
+	Value  uint64     `json:"value"`
+	TxId   string     `json:"txid"`
+	Vout   uint32     `json:"vout"`
+	Status UtxoStatus `json:"status"`
+}
 
+/*
+var request addressUtxoRequest
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+*/
+func GetAddressUtxoWithoutAmount(c *gin.Context) {
+	address := c.Param("address")
+
+	client, err := electrum.NewClientTCP(context.Background(), "node.sathub.io:60601")
 	if err != nil {
 		log.Fatal(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 
 	scriptHash, err := electrum.AddressToElectrumScriptHash(address)
 	if err != nil {
 		log.Fatal("address to script hash error:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"connect electrum error": err.Error()})
+		return
+	}
+
+	utxos, err := client.ListUnspent(context.Background(), scriptHash)
+	if err != nil {
+		log.Fatal("list unspent error:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"list unspent error": err.Error()})
+		return
+	}
+
+	response := make([]mempoolAddressUtxoResponse, 0)
+
+	for _, utxo := range utxos {
+		log.Printf("utxo: %+v", utxo)
+		tx, err := client.GetTransaction(context.Background(), utxo.Hash)
+		if err != nil {
+			continue
+		}
+		if tx.Confirmations == 0 {
+			continue
+		}
+		response = append(response, mempoolAddressUtxoResponse{
+			Value: utxo.Value,
+			TxId:  utxo.Hash,
+			Vout:  utxo.Position,
+			Status: UtxoStatus{
+				BlockHeight: utxo.Height,
+				BlockHash:   tx.Blockhash,
+				BlockTime:   tx.Blocktime,
+				Confirmed:   true}})
+	}
+
+	c.JSON(http.StatusOK, response)
+}
+
+func GetAddressUtxoWithAmount(c *gin.Context) {
+	address := c.Param("address")
+
+	client, err := electrum.NewClientTCP(context.Background(), "node.sathub.io:60601")
+	if err != nil {
+		log.Fatal(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	scriptHash, err := electrum.AddressToElectrumScriptHash(address)
+	if err != nil {
+		log.Fatal("address to script hash error:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"connect electrum error": err.Error()})
 		return
 	}
 
@@ -98,4 +158,18 @@ func GetAddressUtxo(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, response)
+}
+
+func GetAddressUtxo(c *gin.Context) {
+	address := c.Param("address")
+	if address == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "address is required"})
+		return
+	}
+	amount := c.Query("amount")
+	if amount == "" {
+		GetAddressUtxoWithoutAmount(c)
+		return
+	}
+	GetAddressUtxoWithAmount(c)
 }
